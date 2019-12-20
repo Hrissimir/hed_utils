bb800129ad328dd0fe8461d1c1c251104a8b5046
Hrissimir <hrisimir.dakov@gmail.com>
2019-12-21 01:20:18 +0200

    - switched to using module-level constants in os_type.py
    Signed-off-by: Hrissimir <hrisimir.dakov@gmail.com>

--- src/hed_utils/support/__init__.py
+++ src/hed_utils/support/__init__.py
@@ -1,4 +1,13 @@
-from .os_type import OsType
-from . import ps_tool
-from . import text_tool
-from . import time_tool
+from hed_utils.support import log
+from hed_utils.support import os_type
+from hed_utils.support import ps_tool
+from hed_utils.support import text_tool
+from hed_utils.support import time_tool
+
+__all__ = [
+    "log",
+    "os_type",
+    "ps_tool",
+    "text_tool",
+    "time_tool",
+]
--- src/hed_utils/support/os_type.py
+++ src/hed_utils/support/os_type.py
@@ -1,36 +1,33 @@
 import platform
-from enum import Enum, unique
+
+WINDOWS = "Windows"
+LINUX = "Linux"
+MAC_OS = "MacOS"
+UNKNOWN = "Unknown"
 
 
-@unique
-class OsType(Enum):
-    WINDOWS = "Windows"
-    LINUX = "Linux"
-    MAC_OS = "MacOS"
+def is_64bit() -> bool:
+    return ("64bit" in platform.architecture()) or ("64" in platform.uname().machine)
 
-    @staticmethod
-    def is_64bit() -> bool:
-        return ("64bit" in platform.architecture()) or ("64" in platform.uname().machine)
 
-    @staticmethod
-    def is_linux() -> bool:
-        return "linux" in platform.uname().system.lower()
+def is_linux() -> bool:
+    return "linux" in platform.uname().system.lower()
 
-    @staticmethod
-    def is_mac() -> bool:
-        return platform.uname().system in ("Darwin", "macosx")
 
-    @staticmethod
-    def is_windows() -> bool:
-        return "windows" in platform.uname().system.lower()
+def is_mac() -> bool:
+    return platform.uname().system.lower() in ("darwin", "macosx")
 
-    @staticmethod
-    def get_current() -> "OsType":  # pragma: no cover
-        if OsType.is_linux():
-            return OsType.LINUX
-        if OsType.is_mac():
-            return OsType.MAC_OS
-        if OsType.is_windows():
-            return OsType.WINDOWS
 
-        raise OSError("Unknown OS!")
+def is_windows() -> bool:
+    return "windows" in platform.uname().system.lower()
+
+
+def get_current():
+    if is_linux():
+        return LINUX
+    elif is_mac():
+        return MAC_OS
+    elif is_windows():
+        return WINDOWS
+    else:
+        return UNKNOWN
