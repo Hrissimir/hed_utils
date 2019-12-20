8fb5f1cabca502a5d6ceecb423eac81c7d65e267
Hrissimir <hrisimir.dakov@gmail.com>
2019-12-20 03:44:49 +0200

    - added module text_tool.py
    Signed-off-by: Hrissimir <hrisimir.dakov@gmail.com>

---
+++ src/hed_utils/support/text_tool.py
@@ -0,0 +1,124 @@
+import html
+import re
+import sys
+import unicodedata
+
+from typing import List
+
+
+def bytes2human(n):
+    """http://code.activestate.com/recipes/578019
+    >>> bytes2human(10000)
+    '9.8K'
+    >>> bytes2human(100001221)
+    '95.4M'
+    """
+
+    symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols):
+        prefix[s] = 1 << (i + 1) * 10
+    for s in reversed(symbols):
+        if n >= prefix[s]:
+            value = float(n) / prefix[s]
+            return '%.1f%s' % (value, s)
+    return "%sB" % n
+
+
+def find_dates(text: str, datefmt="%Y-%m-%d") -> List[str]:
+    date_patterns = {
+        "%Y-%m-%d": r"(?:\D|^)([12]\d{3}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\d|3[01]))(?:\D|$)"
+    }
+    pattern = date_patterns[datefmt]
+    return list(re.findall(pattern, text))
+
+
+def get_indices(text: str, sub: str) -> List[str]:
+    """Gets all the indexes of occurrence of 'sub' in 'text'"""
+
+    indices = []
+    processed_chars = 0
+    working_part = text
+
+    while sub in working_part:
+        idx = processed_chars + working_part.index(sub)
+        indices.append(idx)
+        processed_chars = idx + len(sub)
+        working_part = text[processed_chars:]
+
+    return indices
+
+
+def html_escape(text, quote=True):
+    return html.escape(text, quote)
+
+
+def html_unescape(text):
+    return html.unescape(text)
+
+
+def invert_quotes(text: str):
+    QUOTE = "'"
+    QUOTES = "\""
+    single_quote_indices = get_indices(text, QUOTE)
+    double_quote_indices = get_indices(text, QUOTES)
+    inverted_chars = []
+    for i in range(len(text)):
+        if i in single_quote_indices:
+            inverted_chars.append(QUOTES)
+        elif i in double_quote_indices:
+            inverted_chars.append(QUOTE)
+        else:
+            inverted_chars.append(text[i])
+    return "".join(inverted_chars)
+
+
+def normalize_spacing(text: str) -> str:
+    """Substitutes all consecutive whitespaces in the given text with a single space."""
+
+    return re.sub(r"\s+", " ", text)
+
+
+def normalize(text, *, map_cmb=True, map_digits=True, map_whitespace=True, form="NFKD") -> str:  # pragma: no-cov
+    text = unicodedata.normalize(form, text)
+
+    if map_cmb:
+        cmb_map = dict.fromkeys(c
+                                for c
+                                in range(sys.maxunicode)
+                                if unicodedata.combining(chr(c)))
+        text = text.translate(cmb_map)
+
+    if map_digits:
+        digits_map = {c: ord("0") + unicodedata.digit(chr(c))
+                      for c
+                      in range(sys.maxunicode)
+                      if unicodedata.category(chr(c)) == "Nd"}
+        text = text.translate(digits_map)
+
+    if map_whitespace:
+        whitespace_map = {ord("\t"): " ",
+                          ord("\f"): " ",
+                          ord("\r"): None}
+        text = text.translate(whitespace_map)
+
+    return text
+
+
+def split_to_lines(text: str, *, strip_text=True, strip_lines=True, keep_empty_lines=False) -> List[str]:
+    if strip_text:
+        text = text.strip()
+
+    lines = text.split("\n")
+
+    if strip_lines:
+        lines = [line.strip() for line in lines]
+
+    if not keep_empty_lines:
+        lines = [line for line in lines if line]
+
+    return lines
+
+
+def split_to_words(text: str) -> List[str]:
+    return re.split(r"\s+", text)
---
+++ tests/test_text_tool.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+
+from hed_utils.support import text_tool
+
+
+class TestTextTool(TestCase):
+
+    def test_bytes2human(self):
+        self.assertEqual(text_tool.bytes2human(10000), "9.8K")
+        self.assertEqual(text_tool.bytes2human(100001221), "95.4M")
+        self.assertEqual(text_tool.bytes2human(2), "2B")
+
+    def test_get_indices_single_letter(self):
+        text = "123aa2a2322abb"
+        self.assertListEqual(text_tool.get_indices(text, "1"), [0])
+        self.assertListEqual(text_tool.get_indices(text, "2"), [1, 5, 7, 9, 10])
+        self.assertListEqual(text_tool.get_indices(text, "a"), [3, 4, 6, 11])
+
+    def test_get_indices_substring(self):
+        text = "123aa2a2322abb"
+        self.assertListEqual(text_tool.get_indices(text, "23"), [1, 7])
+        self.assertListEqual(text_tool.get_indices(text, "bb"), [12])
+
+    def test_invert_quotes(self):
+        text = "asd'asds\"__23123___ A\"'''"
+        inverted_text = "asd\"asds'__23123___ A'\"\"\""
+        self.assertEqual(text_tool.invert_quotes(text), inverted_text)
+
+    def test_normalize_spacing(self):
+        raw_text = "game_start_utc	country	tournament	host_team	guest_team	ratio_1	ratio_X	ratio_2"
+        normalized_text = "game_start_utc country tournament host_team guest_team ratio_1 ratio_X ratio_2"
+        self.assertEqual(text_tool.normalize_spacing(raw_text), normalized_text)
+        raw_text = """a b \nc\td\ne\tf"""
+        self.assertEqual("a b c d e f", text_tool.normalize_spacing(raw_text))
+
+    def test_html_escape_unescape(self):
+        expected = "&lt;"
+        self.assertEqual("&lt;", text_tool.html_escape("<"))
+        self.assertEqual("<", text_tool.html_unescape("&lt;"))
+
+    def test_split_to_lines(self):
+        text_leading_and_trailing_spaces_empty_line_and_line_spacing = \
+            """
+            line1 
+            
+            line2
+            """
+        lines = text_tool.split_to_lines(text_leading_and_trailing_spaces_empty_line_and_line_spacing)
+        self.assertListEqual(lines, ["line1", "line2"])
+        lines = text_tool.split_to_lines(text_leading_and_trailing_spaces_empty_line_and_line_spacing,
+                                         keep_empty_lines=True)
+        self.assertListEqual(lines, ["line1", "", "line2"])
+
+        lines = text_tool.split_to_lines(text_leading_and_trailing_spaces_empty_line_and_line_spacing,
+                                         keep_empty_lines=True, strip_lines=False)
+        self.assertListEqual(lines, ["line1 ", "            ", "            line2"])
+
+        lines = text_tool.split_to_lines(text_leading_and_trailing_spaces_empty_line_and_line_spacing,
+                                         strip_text=False)
+        self.assertListEqual(lines, ["line1", "line2"])
+        lines = text_tool.split_to_lines(text_leading_and_trailing_spaces_empty_line_and_line_spacing,
+                                         strip_text=False, keep_empty_lines=True)
+        self.assertListEqual(lines, ["", "line1", "", "line2", ""])
+
+    def test_split_to_words(self):
+        text = """a \rb \n c \n\n  \t \td"""
+        self.assertListEqual("a b c d".split(), text_tool.split_to_words(text))
+
+    def test_find_dates(self):
+        text = "asd 123 qasd2 2qwa 23 41 a 2019-02-34 sasd 2020-01-23"
+        expected_dates = ["2020-01-23"]
+        self.assertListEqual(expected_dates, text_tool.find_dates(text))
+
+    def test_normalize(self):
+        raw_items = ["Une conservation téléphonique", "Les vacances d'été"]
+        expected_normalized_items = ['Une conservation telephonique', "Les vacances d'ete"]
+        actual_normalized_items = [text_tool.normalize(i) for i in raw_items]
+        self.assertListEqual(expected_normalized_items, actual_normalized_items)
