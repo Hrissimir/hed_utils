282fdbf2e3cbeee50d66f75eef50fa34653718aa
Hrissimir <hrisimir.dakov@gmail.com>
2019-12-23 02:52:26 +0200

    - added profiling of tests execution

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,23 @@
     https://pytest.org/latest/plugins.html
 """
 
+import atexit
+from pathlib import Path
 # import pytest
+from tempfile import gettempdir
+
+from hed_utils.support.profiler import Profiler
+
+STATS_FILE = Path(gettempdir()).joinpath("conftest_profiler_stats.txt")
+
+_profiler = Profiler()
+
+
+@atexit.register
+def save_stats():
+    global _profiler
+    _profiler.disable()
+    STATS_FILE.write_text(_profiler.get_stats(top=0), encoding="utf-8")
+
+
+_profiler.enable()
