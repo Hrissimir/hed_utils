f67e01e8144d243808ea8d9d3df22f77a06d3182
Hrissimir <hrisimir.dakov@gmail.com>
2019-12-24 04:54:02 +0200

    - tweaked imports in hed_utils.selenium.__init__.py
    Signed-off-by: Hrissimir <hrisimir.dakov@gmail.com>

--- src/hed_utils/selenium/__init__.py
+++ src/hed_utils/selenium/__init__.py
@@ -1,6 +1,34 @@
-from hed_utils.selenium.drivers import chrome_driver, opera_driver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+from hed_utils.selenium.defaults import IGNORED_EXCEPTIONS as SELENIUM_EXCEPTIONS
+
+from hed_utils.selenium.drivers import (
+    chrome_driver,
+    opera_driver,
+    DriverWrapper,
+    ThreadLocalDriver
+)
+
+from hed_utils.selenium.elements import (
+    Locator,
+    ElementWrapper,
+    ElementFinder,
+    FindBy
+)
+
+
+# prepare live console with single import 'from hed_utils.selenium import *'
 __all__ = [
+    "By",
+    "Keys",  # element.send_keys("COOL!" + Keys.ENTER)
+    "SELENIUM_EXCEPTIONS",  # try: ... except SELENIUM_EXCEPTIONS:...
     "chrome_driver",
-    "opera_driver"
+    "opera_driver",
+    "DriverWrapper",
+    "ThreadLocalDriver",  # ThreadLocalDriver.set_instance(chrome_driver.create_instance())
+    "Locator",
+    "ElementWrapper",
+    "ElementFinder",
+    "FindBy",  # FindBy.NAME("q").send_keys("selenium")
 ]
--- src/hed_utils/selenium/drivers/__init__.py
+++ src/hed_utils/selenium/drivers/__init__.py
@@ -1,9 +1,11 @@
 from hed_utils.selenium.drivers import chrome_driver
 from hed_utils.selenium.drivers import opera_driver
+from hed_utils.selenium.drivers.driver_wrapper import DriverWrapper
 from hed_utils.selenium.drivers.threadlocal_driver import ThreadLocalDriver
 
 __all__ = [
     "chrome_driver",
     "opera_driver",
+    "DriverWrapper",
     "ThreadLocalDriver"
 ]
