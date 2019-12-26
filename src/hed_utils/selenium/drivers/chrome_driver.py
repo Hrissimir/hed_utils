f02909c6ff08f45ca5d56aca374198877c6943ef
Hrissimir <hrisimir.dakov@gmail.com>
2019-12-26 22:30:37 +0200

    - simplified chrome_driver.py
    Signed-off-by: Hrissimir <hrisimir.dakov@gmail.com>

--- src/hed_utils/selenium/drivers/chrome_driver.py
+++ src/hed_utils/selenium/drivers/chrome_driver.py
@@ -1,9 +1,6 @@
 import atexit
 import logging
-import os
 from pathlib import Path
-from shutil import which
-from typing import Optional
 
 from selenium.webdriver import Chrome, ChromeOptions
 
@@ -11,27 +8,14 @@ _log = logging.getLogger(__name__)
 _log.addHandler(logging.NullHandler())
 
 
-def resolve_executable_path() -> Optional[str]:
-    executable = os.environ.get("webdriver.chrome.driver", which("chromedriver"))
-    if executable:
-        executable_path = Path(executable).resolve()
-        executable = str(executable_path) if executable_path.exists() else None
-    _log.debug("resolved 'chromedriver' executable path: '%s'", executable)
-    return executable
-
-
 def get_options(*, headless=False, user_data_dir=None, downloads_dir=None) -> ChromeOptions:
     """Creates ChromeOptions with some additional values"""
 
     opts = ChromeOptions()
 
-    opts.add_argument("--no-default-browser-check")
     opts.add_argument("--disable-notifications")
     opts.add_argument("--disable-infobars")
     opts.add_argument("--disable-audio-output")
-    opts.add_argument("--disable-login-animations")
-    opts.add_argument("--disable-translate")
-    opts.add_argument("--no-experiments")
     opts.add_argument("--start-maximized")
     opts.add_argument("--test-type=UI")
     opts.add_argument("--window-size=1920,1080")
@@ -52,13 +36,15 @@ def get_options(*, headless=False, user_data_dir=None, downloads_dir=None) -> Ch
     return opts
 
 
-def create_instance(*, headless=False, executable_path=None, user_data_dir=None, downloads_dir=None) -> Chrome:
-    executable_path = executable_path or resolve_executable_path()
-    _log.debug("creating chrome-driver (headless=%s, executable_path='%s', user_data_dir='%s', downloads_dir='%s')",
-               headless, executable_path, user_data_dir, downloads_dir)
+def create_instance(*, headless=False, auto_quit=True, user_data_dir=None, downloads_dir=None) -> Chrome:
+    _log.debug("creating webdriver: Chrome(headless=%s, auto_quit=%s, user_data_dir='%s', downloads_dir='%s')",
+               headless, auto_quit, user_data_dir, downloads_dir)
 
     options = get_options(headless=headless, user_data_dir=user_data_dir, downloads_dir=downloads_dir)
-    driver = Chrome(executable_path=executable_path, options=options) if executable_path else Chrome(options=options)
-    _log.debug("created ChromeDriver instance!")
-    atexit.register(driver.quit)
+    driver = Chrome(options=options)
+    _log.debug("created webdriver instance: Chrome")
+
+    if auto_quit:
+        atexit.register(driver.quit)
+
     return driver
