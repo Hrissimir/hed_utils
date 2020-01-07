b10eead22b7cc31225283b0de56e6b1cbad32fb6
Hrissimir <hrisimir.dakov@gmail.com>
2020-01-07 04:33:53 +0200

    - SharedDriver now singleton
    Signed-off-by: Hrissimir <hrisimir.dakov@gmail.com>

--- src/hed_utils/selenium/drivers/shared_driver.py
+++ src/hed_utils/selenium/drivers/shared_driver.py
@@ -4,26 +4,22 @@ from typing import Optional
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from hed_utils.selenium.drivers.driver_wrapper import DriverWrapper
+from hed_utils.support.singleton import Singleton
 
 _log = logging.getLogger(__name__)
 _log.addHandler(logging.NullHandler())
 
 
-class SharedDriver(DriverWrapper):
-    _instance: Optional[WebDriver] = None
+class SharedDriver(DriverWrapper, metaclass=Singleton):
 
     def __init__(self):
         super().__init__(None)
 
     @classmethod
-    def set_instance(cls, instance: Optional[WebDriver]):
-        _log.debug("setting thread-local driver instance to: %s", repr(instance))
-        cls._instance = instance
+    def set_driver(cls, driver: Optional[WebDriver]):
+        _log.debug("setting thread-local driver instance to: %s", repr(driver))
+        cls()._wrapped_driver = driver
 
     @classmethod
-    def get_instance(cls) -> Optional[WebDriver]:
-        return cls._instance
-
-    @property
-    def wrapped_driver(self) -> Optional[WebDriver]:
-        return self.get_instance()
+    def get_instance(cls) -> "SharedDriver":
+        return cls()
--- tests/test_shared_driver.py
+++ tests/test_shared_driver.py
@@ -6,17 +6,19 @@ from hed_utils.selenium.drivers.shared_driver import SharedDriver
 class SharedDriverTest(TestCase):
 
     def setUp(self) -> None:
-        SharedDriver.set_instance(None)
+        SharedDriver.set_driver(None)
 
-    def test_set_get_instance(self):
-        mock_instance = object()
-        SharedDriver.set_instance(mock_instance)
-        self.assertIs(mock_instance, SharedDriver.get_instance())
+    def test_singleton(self):
+        driver1 = SharedDriver()
+        driver2 = SharedDriver.get_instance()
+        driver3 = SharedDriver().get_instance()
+        self.assertIs(driver1, driver2)
+        self.assertIs(driver3, driver2)
 
     def test_wrapped_driver(self):
-        mock_instance = object()
-        SharedDriver.set_instance(mock_instance)
+        mock_driver = object()
+        SharedDriver.set_driver(mock_driver)
         shared_driver = SharedDriver()
-        self.assertIs(mock_instance, shared_driver.wrapped_driver)
+        self.assertIs(mock_driver, shared_driver.wrapped_driver)
         shared_driver2 = SharedDriver()
-        self.assertIs(shared_driver.wrapped_driver, shared_driver2.wrapped_driver)
+        self.assertIs(mock_driver, shared_driver2.wrapped_driver)
