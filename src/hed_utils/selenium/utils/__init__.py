d817b63737edd6e50ceeea3653dd7633ac474626
Hrissimir <hrisimir.dakov@gmail.com>
2020-03-01 14:06:06 +0200

    - renamed 'js_condition.py' to 'js_conditions.py' and refactored it's contents
    Signed-off-by: Hrissimir <hrisimir.dakov@gmail.com>

--- src/hed_utils/selenium/utils/__init__.py
+++ src/hed_utils/selenium/utils/__init__.py
@@ -1,6 +1,6 @@
 from hed_utils.selenium.utils.element_finder import ElementFinder
 from hed_utils.selenium.utils.find_by import FindBy
-from hed_utils.selenium.utils.js_condition import wait_for_page_load
+from hed_utils.selenium.utils.js_conditions import wait_for_page_load
 from hed_utils.selenium.utils.shared_driver import SharedDriver
 
 __all__ = [
