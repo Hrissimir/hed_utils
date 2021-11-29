1e06b1092f96583c603589e6c82da3cb60273b5f
Hrissimir <hrisimir.dakov@gmail.com>
2021-11-30 00:30:44 +0200

    updated removed singleton meta from shared_driver.py

--- src/hed_utils/selenium/utils/shared_driver.py
+++ src/hed_utils/selenium/utils/shared_driver.py
@@ -4,13 +4,13 @@ from typing import Optional
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from hed_utils.selenium.wrappers.driver_wrapper import DriverWrapper
-from hed_utils.support.singleton import Singleton
 
 _log = logging.getLogger(__name__)
 _log.addHandler(logging.NullHandler())
 
 
-class SharedDriver(DriverWrapper, metaclass=Singleton):
+class SharedDriver(DriverWrapper):
+    _SHARED_INSTANCE = None
 
     def __init__(self):
         super().__init__(None)
@@ -18,4 +18,8 @@ class SharedDriver(DriverWrapper, metaclass=Singleton):
     @classmethod
     def set_instance(cls, driver: Optional[WebDriver]):
         _log.debug("setting shared driver instance to: %s", repr(driver))
-        cls()._wrapped_driver = driver
+        cls._SHARED_INSTANCE = driver
+
+    @property
+    def wrapped_driver(self) -> WebDriver:
+        return self._SHARED_INSTANCE
