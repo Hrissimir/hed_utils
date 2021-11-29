565d2eeab722cd06e4a6bbc19a95bd5dcae4ac48
Hrissimir <hrisimir.dakov@gmail.com>
2021-11-30 00:31:01 +0200

    updated removed singleton meta from shared_driver.py

--- tests/test_shared_driver.py
+++ tests/test_shared_driver.py
@@ -8,12 +8,18 @@ class SharedDriverTest(TestCase):
     def setUp(self) -> None:
         SharedDriver.set_instance(None)
 
-    def test_singleton(self):
-        driver1 = SharedDriver()
-        driver2 = SharedDriver()
-        self.assertIs(driver1, driver2)
-
     def test_wrapped_driver(self):
         mock_driver = object()
         SharedDriver.set_instance(mock_driver)
         self.assertIs(mock_driver, SharedDriver().wrapped_driver)
+
+    def test_instance_is_shared(self):
+        driver1 = SharedDriver()
+        driver2 = SharedDriver()
+        self.assertIsNone(driver1.wrapped_driver)
+        self.assertIsNone(driver2.wrapped_driver)
+
+        instance = object()
+        SharedDriver.set_instance(instance)
+        self.assertIs(instance, driver1.wrapped_driver)
+        self.assertIs(instance, driver2.wrapped_driver)
